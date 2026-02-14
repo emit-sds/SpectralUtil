736a22bc5737f7f9494e32547d1bf117c1f483ac
brodrick <phil.brodrick@gmail.com>
2026-02-14 12:12:20 -0800

    readme and main mods

--- spectral_util/__main__.py
+++ spectral_util/__main__.py
@@ -10,8 +10,8 @@ from spectral_util.ea_assist import earthaccess_helpers_AV3, earthaccess_helpers
 def cli():
     pass
 
-cli.add_command(earthaccess_helpers_AV3.find_download_and_combine, name='av3_download')
-cli.add_command(earthaccess_helpers_EMIT.find_download_and_combine_EMIT, name='emit_download')
+cli.add_command(earthaccess_helpers_AV3.find_download_and_combine, name='av3-download')
+cli.add_command(earthaccess_helpers_EMIT.find_download_and_combine_EMIT, name='emit-download')
 cli.add_command(mosaic.cli, name='mosaic')
 cli.add_command(quicklooks.cli, name='quicklooks')
 
