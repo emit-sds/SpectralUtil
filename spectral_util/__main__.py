209044aeb6e8d184e81204c99aa7694aee5805c4
Phil Brodrick <phil.brodrick@gmail.com>
2026-02-15 12:58:01 -0500

    basic fid download utility

--- spectral_util/__main__.py
+++ spectral_util/__main__.py
@@ -5,13 +5,13 @@ import click
 from spectral_util.common import quicklooks
 from spectral_util.mosaic import mosaic
 from spectral_util.ea_assist import earthaccess_helpers_AV3, earthaccess_helpers_EMIT
+from spectral_util import ea_assist
 
 @click.group()
 def cli():
     pass
 
-cli.add_command(earthaccess_helpers_AV3.find_download_and_combine, name='av3-download')
-cli.add_command(earthaccess_helpers_EMIT.find_download_and_combine_EMIT, name='emit-download')
+cli.add_command(ea_assist.cli, name='download')
 cli.add_command(mosaic.cli, name='mosaic')
 cli.add_command(quicklooks.cli, name='quicklooks')
 
